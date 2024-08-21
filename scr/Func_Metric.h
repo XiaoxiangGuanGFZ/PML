79b167bb7b6c8994f1b0830efc2addc555967d92
Xiaoxiang Guan <96783365+XiaoxiangGuanGFZ@users.noreply.github.com>
2024-08-21 17:24:54 +0200

    evaluation metrics: NSE ..

---
+++ scr/Func_Metric.h
@@ -0,0 +1,20 @@
+#ifndef FUNC_METRIC
+#define FUNC_METRIC
+
+void Metrics(
+    double *sim, double *obs, int counts,
+    double *nse,
+    double *R2,
+    double *RMSE,
+    double *Re
+);
+
+double NSE(double *sim, double *obs, int counts);
+
+double RootMeanSquareError(double *sim, double *obs, int counts);
+
+double RelativeError(double *sim, double *obs, int counts);
+
+double R_squared(double *sim, double *obs, int counts);
+
+#endif
